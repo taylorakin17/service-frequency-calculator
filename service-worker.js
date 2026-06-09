5dcb1c50e4310904718b75010b732fc59fdfbbb5
Taylor Akin <takin@princeton.edu>
2026-06-08 22:18:02 -0500

    Bump SW cache to v2 to force fresh fetch after update

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = 'v1';
+const CACHE_VERSION = 'v2';
 const CACHE_NAME = `service-frequency-${CACHE_VERSION}`;
 
 // Derive the base path from where the service worker lives so this works
@@ -16,7 +16,9 @@ const STATIC_ASSETS = [
 
 self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(STATIC_ASSETS))
+    caches.delete(CACHE_NAME)
+      .then(() => caches.open(CACHE_NAME))
+      .then((cache) => cache.addAll(STATIC_ASSETS))
   );
   self.skipWaiting();
 });
